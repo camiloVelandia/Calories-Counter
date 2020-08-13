bea0898434e7bda40e924af2c2b13a2703d99c60
Camilo Velandia <velandiacamilo356@gmail.com>
2020-08-12 20:52:40 -0500

    add validation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ let description = document.getElementById("description");
 let carbs = document.getElementById("carbs");
 let calories = document.getElementById("calories");
 let protein = document.getElementById("protein");
+
+const addBtn = document.querySelector(".addButton");
+
+addBtn.addEventListener("click", (e) => {
+  description.value === "" ? description.classList.add("is-invalid") : null;
+  carbs.value === "" ? carbs.classList.add("is-invalid") : null;
+  calories.value === "" ? calories.classList.add("is-invalid") : null;
+  protein.value === "" ? protein.classList.add("is-invalid") : null;
+
+  if (description.value && carbs.value && calories.value && protein.value) {
+    console.log("ok");
+  }
+});
+
+description.addEventListener("keypress", (e) => {
+  description.classList.remove("is-invalid");
+});
+carbs.addEventListener("keypress", (e) => {
+  carbs.classList.remove("is-invalid");
+});
+calories.addEventListener("keypress", (e) => {
+  calories.classList.remove("is-invalid");
+});
+protein.addEventListener("keypress", (e) => {
+  protein.classList.remove("is-invalid");
+});
